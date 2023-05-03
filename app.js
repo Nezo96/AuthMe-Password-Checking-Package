17880e61629f6ffdd90693257a879d2a23dce33a
Nezo96 <racradoslav96@gmail.com>
2023-05-03 15:20:44 +0200

    Package Init

--- app.js
+++
@@ -1,8 +0,0 @@
-import checkPassword from "./index.js";
-
-const dbPass =
-  "$SHA$dc991e87ddb674f1$9c27ce343ccee87be3dca9c1f7aac79abe2f71abd7cb2825006f0a64bafdd361";
-const pass = "test";
-
-const passss = checkPassword(pass, dbPass);
-console.log(passss);
